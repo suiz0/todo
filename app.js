6435dd73c49d77d2b6a829742c42b9ccd4e55c1b
suiz0 <suiz0@gula.(none)>
2017-03-23 21:54:22 -0600

    Remove commented code

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ var Backbone =  require('backbone');
 var Todos =  require('./src/collections/todos');
 var Todo = require('./src/models/todo');
 var MasterView =  require('./src/views/todo-master');
-var Notification =  require('./src/notification/index.js');
+var Notification =  require('./src/notification/index');
 
 Backbone.$ = jquery;
 
-//jquery(function() {
-    var records = new Todos();
-    var appView = new MasterView({collection: records, result: new Todos(), model: new Todo()});
-    var notif = new Notification({collection: records});
+var records = new Todos();
+var appView = new MasterView({collection: records, result: new Todos(), model: new Todo()});
+var notif = new Notification({collection: records});
 
-    records.fetch();
-//});
+records.fetch();
